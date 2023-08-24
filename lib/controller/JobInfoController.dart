f12497fa3ced3218ab3ad1b417c733e9fce6b7d1
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2023-08-24 15:33:08 +0900

    [feat-#9]알바명 조회 api작성 (#10)

--- lib/controller/JobInfoController.dart
+++ lib/controller/JobInfoController.dart
@@ -4,13 +4,13 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:ok_client/responsebody/JobInfoResponseBody.dart';
 
-
+var URL = 'http://172.30.1.34:8080';
 
 Future<JobInfoResponseBody> Jobinfo_save(JobInfoRequestBody requestBody) async{
   var responseBody = JobInfoResponseBody(id: 0, name: "0", result: true);
   var response = await http.post(
     //안드로이드 기기에서 작동시 localhost 대신 무선LAN IPv4값으로 수정
-    Uri.parse('http://172.30.1.34:8080/jobinfo/save'),
+    Uri.parse('http://localhost:8080/jobinfo/save'),
     headers: {
       "Access-Control-Allow-Origin": "*",
       'Content-Type': 'application/json',
@@ -42,11 +42,42 @@ Future<JobInfoResponseBody> Jobinfo_save(JobInfoRequestBody requestBody) async{
   else{
     return responseBody;
   }
+}
+
+Future<List<String>> show_names(String userId)async{
+  List<String> nameList = [];
+
+  Map<String,String> params={
+    "id":userId
+  };
+
+  var url = Uri.http('http://localhost:8080','/jobinfo/name',params);
 
 
+  var response = await http.get(
+    //안드로이드 기기에서 작동시 localhost 대신 무선LAN IPv4값으로 수정
+    url,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'Accept': '*/*'
+    },
+  );
 
+
+  if(response.statusCode == 200){
+    print("ok200");
+    nameList = jsonDecode(response.body);
+    print(nameList);
+    return nameList;
+  }
+  else{
+    return nameList;
+  }
 
 }
+
+
 
 List<Map<String,String>> listToJson(List<DayAndTime> list){
   List<Map<String,String>> items = [];
